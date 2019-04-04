459bb3f68d9a4da368e262928d0a04ca70aef411
Thaynan Andrey <thaynan.nunes@ccc.ufcg.edu.br>
2019-04-03 22:44:09 -0300

    Adicionado atributos: title, auhor, section, url e data (parcialmente)

--- ri_lab_01/spiders/carta_capital.py
+++ ri_lab_01/spiders/carta_capital.py
@@ -2,6 +2,8 @@
 import scrapy
 import json
 
+from datetime import datetime
+
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
 
@@ -21,16 +23,19 @@ class CartaCapitalSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        # Obtém conteúdos dos artigos de cada página
-        if(response.css('article').get() is not None):
+        # Obtém conteúdos dos artigos de cada página    
+        if(self.__is_response_article(response) and self.__is_valid_article(response)):
             yield {
-                'title': response.css('h1.eltdf-title-text::text').get(),
-                'author': response.css('div.eltdf-post-info-author a::text').get(),
+                'title': self.__get_article_title(response),
+                'author': self.__get_article_author(response),
+                'date': self.__get_article_date(response),
+                'section': self.__get_article_section(response),
+                'url': self.__get_article_url(response),
             }
 
         # Obtém novas páginas
-        for next_page in response.css('a::attr(href)').getAll():
-            if self.__isValidLink(next_page):
+        for next_page in response.css('a::attr(href)').getall():
+            if self.__is_valid_link(next_page):
                 self.visited_urls.append(next_page)
                 # next_page = response.urljoin(next_page)
                 yield scrapy.Request(next_page, callback=self.parse)
@@ -41,5 +46,41 @@ class CartaCapitalSpider(scrapy.Spider):
             f.write(response.body)
         self.log('Saved file %s' % filename)
 
-    def __isValidLink(self, link):
-        return (link is not None) and (self.visited_urls.count(link) == 0) and (link in 'https://www.cartacapital.com.br/economia')
+    def __is_response_article(self, response):
+        return response.css('article').get() is not None
+
+    def __is_valid_article(self, page_article):
+        string_date = page_article.css('div.eltdf-post-info-date a::text').get()
+        year = int(string_date.split(" de ")[2])
+        return year == 2019
+
+    def __is_valid_link(self, link):
+        return (link is not None) and (self.visited_urls.count(link) == 0) and ('https://www.cartacapital.com.br/economia' in link or 'https://www.cartacapital.com.br/sociedade' in link or 'https://www.cartacapital.com.br/politica' in link or 'https://www.cartacapital.com.br/justica' in link or 'https://www.cartacapital.com.br/mundo' in link)
+
+    def __get_article_title(self, response):
+        return response.css('h1.eltdf-title-text::text').get()
+
+    def __get_article_author(self, response):
+        return response.css('div.eltdf-title-post-author-info a::text').get()
+
+    def __get_article_date(self, response):
+        '''monthes = {"janeiro": "01", "fevereiro": "02", "março": "03", "abril": "04", "maio": "05"}
+        string_date = response.css('div.eltdf-post-info-date a::text').get()
+
+        list_date_details = string_date.split(" de ")
+
+        day = list_date_details[0]
+        month = monthes[list_date_details[1]]
+        year = list_date_details[2]
+        formatted_date = "/".join([day, month, year])
+
+        article_date = datetime.strptime(formatted_date, '%d/%m/%Y')
+        print article_date
+        print type(article_date)'''
+        return response.css('div.eltdf-post-info-date a::text').get()
+
+    def __get_article_section(self, response):
+        return response.css('div.eltdf-post-info-category a::text').get()
+
+    def __get_article_url(self, response):
+        return response.request.url
